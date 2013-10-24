b202b8133003b6d3ded61aa503a4640cfc73e596
Raymond Roestenburg <raymond.roestenburg@gmail.com>
2013-10-24 13:47:07 +0200

    more docs

--- src/main/scala/com/xebia/exercise3/ReverseActor.scala
+++ src/main/scala/com/xebia/exercise3/ReverseActor.scala
@@ -28,16 +28,25 @@ class ReverseActor extends Actor {
 
   import context._
 
-  //TODO send Init to self
+  //TODO send Init to self (right here or override preStart)
 
   // TODO the receive method should be set to the uninitialized Receive function
 
-  def receive = uninitialized
+  def receive =
 
   // TODO create an uninitialized Receive method
+  // Send back NotInitialized on a Reverse message.
+  // Load the AsyncReverseFunction using the ReverserFactory on receiving Init.
+  // call become to transition to initialized state once the Future completes successfully.
+  // pass through the AsyncReverseFunction to the initialized Receive function instead of using a var.
+
   def uninitialized:Receive = {
   }
 
+  //TODO Implement the initialized Receive function:
+  // call the AsyncReverseFunction,
+  // on completion of the Future send back to a **captured sender**
+  // (do not close over sender but create a val which contains the sender at the time of receiving the Reverse message).
   def initialized(reverse: AsyncReverseFunction):Receive = {
   }
 
